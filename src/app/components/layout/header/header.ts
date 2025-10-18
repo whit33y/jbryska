cb9415811a7ea18466ca8a4948fcfb109a6edae7
whit33y <troppout@gmail.com>
2025-10-18 22:56:01 +0200

    feat: added header with styles and changing theme function

--- src/app/components/layout/header/header.ts
+++ src/app/components/layout/header/header.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ThemeService } from '../../../services/theme-service';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './header.html',
-  styleUrl: './header.scss'
+  styleUrl: './header.scss',
 })
 export class Header {
+  private elementRef = inject(ElementRef);
+  private themeService = inject(ThemeService);
+  menuOpen = false;
+  theme = this.themeService.theme;
 
+  changeTheme() {
+    this.themeService.toggleTheme();
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+  }
+
+  @HostListener('document:click', ['$event'])
+  handleClickOutside(event: Event) {
+    const target = event.target as HTMLElement;
+    const clickedInside = this.elementRef.nativeElement.contains(target);
+    if (!clickedInside && this.menuOpen) {
+      this.closeMenu();
+    }
+  }
 }
