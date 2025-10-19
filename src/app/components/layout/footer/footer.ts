fb642afa4d8e20950539f9667a87e46cff3338df
whit33y <troppout@gmail.com>
2025-10-19 17:06:19 +0200

    feat: added footer with styles

--- src/app/components/layout/footer/footer.ts
+++ src/app/components/layout/footer/footer.ts
@@ -1,11 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.html',
-  styleUrl: './footer.scss'
+  styleUrl: './footer.scss',
 })
 export class Footer {
+  transformStyle = 'scale(1) translate(0, 0)';
 
+  onMouseMove(event: MouseEvent) {
+    const footer = (event.currentTarget as HTMLElement).querySelector('.container') as HTMLElement;
+    const rect = footer.getBoundingClientRect();
+    const x = event.clientX - (rect.left + rect.width / 2);
+    const y = event.clientY - (rect.top + rect.height / 2);
+
+    this.transformStyle = `scale(1.05) translate(${x * 0.05}px, ${y * 0.05}px)`;
+  }
+
+  onMouseLeave() {
+    this.transformStyle = 'scale(1) translate(0, 0)';
+  }
 }
