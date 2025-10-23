6d6865ab95fb23918efbeeff79c4e6ffadcf04ef
Jakub Bryska <troppout@gmail.com>
2025-10-23 13:20:52 +0200

    change: styles on page components

---
+++ src/app/services/project.interface.ts
@@ -0,0 +1,10 @@
+export interface Project {
+  name: string;
+  description: string;
+  link: string;
+  technologies: string[];
+  screenshots: string[];
+  livePreview: string;
+  githubLink: string;
+  status: string;
+}
