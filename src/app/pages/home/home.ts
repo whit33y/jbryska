4f2d3afa32582102dd4eb856119fd39c855ab7a3
whit33y <troppout@gmail.com>
2025-10-23 07:45:13 +0200

    change: jpg to webp, ngOptimized instead of regular img, alternative texts

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,8 +1,9 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
