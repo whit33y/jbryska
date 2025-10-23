bc06ac043ee45bd9d309a1c4a54b5b51b9d97324
whit33y <troppout@gmail.com>
2025-10-23 07:45:13 +0200

    change: jpg to webp, ngOptimized instead of regular img, alternative texts

--- src/app/pages/projects/projects.ts
+++ src/app/pages/projects/projects.ts
@@ -1,8 +1,9 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './projects.html',
   styleUrl: './projects.scss',
 })
@@ -14,7 +15,7 @@ export class Projects {
         'Moja pierwsza pełnoprawna aplikacja webowa typu full-stack do zarządzania i śledzenia zadań serwisowych — od wymiany oleju w samochodzie po regularne prace domowe.',
       link: 'https://github.com/whit33y/maintenance-frontend',
       technologies: ['Angular', 'Typescript', 'Node.js', 'Express', 'Tailwind', 'MySQL'],
-      screenshots: ['ss/maintenance2.png', 'ss/maintenance1.png'],
+      screenshots: ['ss/maintenance2.webp', 'ss/maintenance1.webp'],
       livePreview: 'https://maintenance-frontend-black.vercel.app/',
       githubLink: 'https://github.com/whit33y/maintenance-frontend',
       status: 'W toku',
@@ -25,7 +26,7 @@ export class Projects {
         'AngularEcom to prosta i responsywna aplikacja e-commerce stworzona w Angularze. Użytkownik może przeglądać produkty z widokami szczegółowymi, dodawać je do koszyka i bezpiecznie finalizować zamówienia przez Stripe. Administrator ma możliwość zarządzania użytkownikami, produktami i kategoriami poprzez panel administracyjny.',
       link: 'https://github.com/whit33y/AngularEcom',
       technologies: ['Angular', 'Typescript', 'Tailwind', 'Supabase'],
-      screenshots: ['ss/angularecom1.png', 'ss/angularecom2.png'],
+      screenshots: ['ss/angularecom1.webp', 'ss/angularecom2.webp'],
       livePreview: '',
       githubLink: 'https://github.com/whit33y/AngularEcom',
       status: 'Gotowy',
@@ -36,7 +37,7 @@ export class Projects {
         'ITHelper to responsywna aplikacja webowa stworzona w celu uproszczenia zarządzania zgłoszeniami pomiędzy użytkownikami a administratorami.',
       link: 'https://github.com/whit33y/ITHelper',
       technologies: ['Angular', 'Typescript', 'Tailwind', 'Appwrite'],
-      screenshots: ['ss/ithelper1.png', 'ss/ithelper2.png'],
+      screenshots: ['ss/ithelper1.webp', 'ss/ithelper2.webp'],
       livePreview: 'https://it-helper-hkiw.vercel.app/login',
       githubLink: 'https://github.com/whit33y/ITHelper',
       status: 'Gotowy',
@@ -47,7 +48,7 @@ export class Projects {
         'PRBook to aplikacja webowa służąca do śledzenia wyników i rekordów w sportach wytrzymałościowych oraz na siłowni.',
       link: 'https://github.com/whit33y/PRBook',
       technologies: ['Angular', 'Typescript', 'Storybook', 'Appwrite'],
-      screenshots: ['ss/prbook1.png', 'ss/prbook2.png'],
+      screenshots: ['ss/prbook1.webp', 'ss/prbook2.webp'],
       livePreview: '',
       githubLink: 'https://github.com/whit33y/PRBook',
       status: 'Gotowy',
@@ -58,7 +59,7 @@ export class Projects {
         'AngularWeatherApp to prosta aplikacja pogodowa, w której można sprawdzić prognozę na 3, 7 lub 10 dni dla wybranego miasta.',
       link: 'https://github.com/whit33y/angular-weatherapp',
       technologies: ['Angular', 'Typescript', 'Tailwind'],
-      screenshots: ['ss/angular-weather1.png', 'ss/angular-weather2.png'],
+      screenshots: ['ss/angular-weather1.webp', 'ss/angular-weather2.webp'],
       livePreview: '',
       githubLink: 'https://github.com/whit33y/angular-weatherapp',
       status: 'Gotowy',
