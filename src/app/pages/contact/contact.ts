f44c32003b2142f9781f900a736e63fa3b70dc19
Jakub Bryska <troppout@gmail.com>
2025-10-24 09:02:26 +0200

    feat: added robots.txt and meta descriptions + small fixes

--- src/app/pages/contact/contact.ts
+++ src/app/pages/contact/contact.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { GithubIconComponent } from '../../components/elements/icons/github-icon';
 import { LinkedinIconComponent } from '../../components/elements/icons/linkedin-icon';
 import { MailIconComponent } from '../../components/elements/icons/mail-icon';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-contact',
@@ -9,4 +10,15 @@ import { MailIconComponent } from '../../components/elements/icons/mail-icon';
   templateUrl: './contact.html',
   styleUrl: './contact.scss',
 })
-export class Contact {}
+export class Contact implements OnInit {
+  private meta = inject(Meta);
+  private title = inject(Title);
+
+  ngOnInit() {
+    this.title.setTitle('Kontakt - Jakub Bryska');
+    this.meta.updateTag({
+      name: 'description',
+      content: 'Skontaktuj się ze mną przez jeden z wybranych przez Ciebie kanałów.',
+    });
+  }
+}
